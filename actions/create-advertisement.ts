0d12278ccb967d33e0454c70ba7778c826cd5b53
Giuliano Varriale <giuliano.oca@gmail.com>
2025-04-04 17:27:01 +0200

    feat: display success and error messages

---
+++ actions/create-advertisement.ts
@@ -0,0 +1,5 @@
+import { AdvertisementForm } from "@/components/ui/new-advertisement-form/advertisement-schema";
+
+export async function createAdvertisement(_: AdvertisementForm) {
+  console.log("advertisement created");
+}
