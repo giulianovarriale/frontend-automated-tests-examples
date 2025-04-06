cd3d4ee5d1b5bb39d64e6b7c80a4583db9cb15e9
Giuliano Varriale <giuliano.oca@gmail.com>
2025-04-06 19:48:21 +0200

    test: setup testing library style selector

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,4 +1,8 @@
 /// <reference types="cypress" />
+/// <reference types="@testing-library/cypress" />
+
+import "@testing-library/cypress/add-commands";
+
 // ***********************************************
 // This example commands.ts shows you how to
 // create various custom commands and overwrite
