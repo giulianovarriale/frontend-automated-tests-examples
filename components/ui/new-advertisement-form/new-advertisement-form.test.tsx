b6222bffe06018a7548e761d6440de4126ee4040
Giuliano Varriale <giuliano.oca@gmail.com>
2025-04-11 11:30:43 +0200

    test: errors are displayed for invalid fields

--- components/ui/new-advertisement-form/new-advertisement-form.test.tsx
+++ components/ui/new-advertisement-form/new-advertisement-form.test.tsx
@@ -1,5 +1,6 @@
 import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
 import { NewAdvertisementForm } from "./new-advertisement-form";
 
 test("fields are in the document", () => {
@@ -8,6 +9,44 @@ test("fields are in the document", () => {
   expect(screen.getByLabelText(/title/i)).toBeInTheDocument();
   expect(screen.getByLabelText(/description/i)).toBeInTheDocument();
   expect(screen.getByLabelText(/price/i)).toBeInTheDocument();
-
   expect(screen.getByLabelText(/allow bidding/i)).toBeInTheDocument();
 });
+
+test("display error messages when required fields are empty", async () => {
+  render(<NewAdvertisementForm />);
+
+  const submitButton = screen.getByRole("button", {
+    name: /post advertisement/i,
+  });
+
+  userEvent.click(submitButton);
+
+  expect(await screen.findByText(/title is required/i)).toBeInTheDocument();
+
+  expect(
+    await screen.findByText(/description is required/i)
+  ).toBeInTheDocument();
+
+  expect(
+    await screen.findByText(/price must be at least \$0.50/i)
+  ).toBeInTheDocument();
+});
+
+test.each(["abc", "10a", "10,99"])(
+  "display error message when price format is invalid (%s)",
+  async (value) => {
+    render(<NewAdvertisementForm />);
+
+    await userEvent.type(screen.getByLabelText(/price/i), value);
+
+    await userEvent.click(
+      screen.getByRole("button", {
+        name: /post advertisement/i,
+      })
+    );
+
+    expect(
+      await screen.findByText(/please enter a valid price \(e.g 99.99\)/i)
+    ).toBeInTheDocument();
+  }
+);
