0b227f16c9034559fb5dd0b34262ebd93f1cadd2
Giuliano Varriale <giuliano.oca@gmail.com>
2025-04-11 11:30:43 +0200

    test: implement e2e test for posting an advertisement

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,25 @@
 import Link from "next/link";
 import { ShoppingBag } from "lucide-react";
+import { Button } from "./ui/button";
 
 export function Header() {
   return (
     <header className="border-b">
-      <div className="container mx-auto h-16 py-4">
+      <div className="container mx-auto h-16 py-4 flex items-center justify-between">
         <Link href="/" className="flex items-center gap-2">
           <ShoppingBag className="h-6 w-6" />
           <span className="text-xl font-bold">Marketplace</span>
         </Link>
+
+        <div className="flex gap-2">
+          <Link href="/" passHref>
+            <Button variant="outline">Browse advertisements</Button>
+          </Link>
+
+          <Link href="/advertisements/new" passHref>
+            <Button>New advertisement</Button>
+          </Link>
+        </div>
       </div>
     </header>
   );
