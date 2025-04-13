2201f02e32026911e15031a9825b582171ec02fa
Giuliano Varriale <giuliano.oca@gmail.com>
2025-04-13 16:59:51 +0200

    Fix lint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type React from "react";
 import "@/app/globals.css";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Header } from "@/components/header";
 
