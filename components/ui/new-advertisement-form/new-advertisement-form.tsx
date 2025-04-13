f66f557f2ea3d959514d05870969f1acc9742383
Giuliano Varriale <giuliano.oca@gmail.com>
2025-04-13 16:59:51 +0200

    Fix lint

--- components/ui/new-advertisement-form/new-advertisement-form.tsx
+++ components/ui/new-advertisement-form/new-advertisement-form.tsx
@@ -1,10 +1,3 @@
-import { Checkbox } from "@/components/ui/checkbox";
-import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button";
-import { FieldHelper } from "@/components/ui/field-helper";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
-
 export function NewAdvertisementForm() {
   return <p>Form here...</p>;
 }
